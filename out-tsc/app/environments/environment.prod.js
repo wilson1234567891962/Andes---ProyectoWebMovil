671aa948bec9a63e2e2977650bac0ecfdd27bbb1
wilson1234567891962 <wilson1234567891962@users.noreply.github.com>
2023-04-04 16:43:34 +0000

    deploy: 4a3c73c80ed0dc16c1c066f92fbc9fb3163c272a

--- out-tsc/app/environments/environment.prod.js
+++
@@ -1,5 +0,0 @@
-export const environmentProd = {
-    production: true,
-    URL_PRODUCTION: 'https://andes-deploy.herokuapp.com/ANDES/'
-};
-//# sourceMappingURL=environment.prod.js.map
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environmentProd = {
-  production: true,
-  URL_PRODUCTION: 'https://miso-proyecto2-backend.herokuapp.com/ANDES/'
-};
